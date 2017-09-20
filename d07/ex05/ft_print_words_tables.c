711e8cc409b89db3a10b6649471c76ab467a5dfa
Martin Schneider <mschneid@z4r2p6.le-101.fr>
2017-09-20 17:56:09 +0200

    kikoo

--- d07/ex05/ft_print_words_tables.c
+++ d07/ex05/ft_print_words_tables.c
@@ -1,34 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_print_words_tables.c                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mschneid <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/09/19 20:26:58 by mschneid          #+#    #+#             */
-/*   Updated: 2017/09/20 16:54:58 by mschneid         ###   ########.fr       */
+/*   Created: 2017/09/20 16:34:18 by mschneid          #+#    #+#             */
+/*   Updated: 2017/09/20 17:28:00 by mschneid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+void	ft_putchar(char c);
 
-int		ft_count_words(char *str);
-
-char	**ft_split_whitespaces(char *str);
-
-int		main(void)
+void	ft_putstr(char *str)
 {
-	char **result;
-	char *str = "qwe kldjls jlfdsjl dfs fds fdou8 u8r338\nfnsf@#$ dsfd sd f";
 	int	i;
 
 	i = 0;
-	result = ft_split_whitespaces(str);
-	printf("%d\n", ft_count_words(str));
-	while (i < ft_count_words(str))
+	while (str[i] != '\0')
 	{
-		printf("%d = %s\n", i, result[i]);
+		ft_putchar(str[i]);
 		i++;
 	}
-	return (0);
+}
+
+void	ft_print_words_tables(char **tab)
+{
+	int	i;
+
+	i = 0;
+	while (tab[i])
+	{
+		ft_putstr(tab[i]);
+		ft_putchar('\n');
+		i++;
+	}
 }
--- d07/ex05/main.c
+++ d07/ex05/main.c
@@ -5,30 +5,26 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mschneid <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/09/19 20:26:58 by mschneid          #+#    #+#             */
-/*   Updated: 2017/09/20 16:54:58 by mschneid         ###   ########.fr       */
+/*   Created: 2017/09/20 16:58:32 by mschneid          #+#    #+#             */
+/*   Updated: 2017/09/20 17:49:14 by mschneid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include <unistd.h>
 
-int		ft_count_words(char *str);
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
 
 char	**ft_split_whitespaces(char *str);
 
+void	ft_print_words_tables(char **tab);
+
 int		main(void)
 {
-	char **result;
-	char *str = "qwe kldjls jlfdsjl dfs fds fdou8 u8r338\nfnsf@#$ dsfd sd f";
-	int	i;
+	char str[] = "salut les zouzous comment allez vous      fdsfljdsjl fdsfds";
 
-	i = 0;
-	result = ft_split_whitespaces(str);
-	printf("%d\n", ft_count_words(str));
-	while (i < ft_count_words(str))
-	{
-		printf("%d = %s\n", i, result[i]);
-		i++;
-	}
+	ft_print_words_tables(ft_split_whitespaces(str));
 	return (0);
 }
